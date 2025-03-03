8f7d24e8af28671ca6ae71ba911f5fdfbe8d1f7a
mohammad subkhan <51844971+masbekkk@users.noreply.github.com>
2025-03-03 08:37:00 +0700

    add env

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ io.on('connection', (socket) => {
     });
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-    console.log(`Socket.io WebSocket server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
